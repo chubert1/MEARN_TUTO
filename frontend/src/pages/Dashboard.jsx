441acff94cc225b8d286eff9aec46190ae041f96
chubert1 <hubert.decroocq@laposte.net>
2022-08-23 11:28:24 +0200

    dashbord

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 
-
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 function Dashboard() {
   return (
     <div>Dashboard</div>
